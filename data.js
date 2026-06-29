8d4a1930aaeeaacb3a711fa33d25a41ce97cf88e
Omoyeni <tobi.omoyeni@accenture.com>
2026-06-29 11:57:13 +0100

    updated wdio config path to SIT tests

---
+++ data.js
@@ -0,0 +1,29 @@
+export default {
+  bankdetails: {
+    'Account name': 'Glamshire County Council',
+    'Sort code': '12 34 56',
+    'Account number': '78910112'
+  },
+  gridContent: [
+    {
+      title: 'Payment documents',
+      description: 'You can view and download your:',
+      listItems: [
+        'documents relating to your grant funding, assessments and payments'
+      ]
+    },
+    {
+      title: 'Bank details',
+      description: 'You can:',
+      listItems: ['view bank details']
+    },
+    {
+      title: 'Get help and guidance',
+      description: 'You can view our contact details and find links to:',
+      listItems: [
+        'read Local Authority Payments (LAPs) guidance',
+        'raise a support request'
+      ]
+    }
+  ]
+}
